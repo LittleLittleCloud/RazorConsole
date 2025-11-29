bd5225a0fe3c61a01f0c7c4c61efa7d5c89f8b1f
FallenParadise <yjokerch@gmail.com>
2025-11-30 02:08:22 +0300

    Addressing PR comments

--- src/RazorConsole.Tests/Core/Utilities/AnsiSequencesTests.cs
+++ src/RazorConsole.Tests/Core/Utilities/AnsiSequencesTests.cs
@@ -6,21 +6,25 @@ namespace RazorConsole.Tests.Utilities;
 
 public sealed class AnsiSequencesTests
 {
+    private const string IndexEscapeSequence = AnsiSequences.ESC + "D";
+    private const string NextLineEscapeSequence = AnsiSequences.ESC + "E";
+    private const string ReverseIndexEscapeSequence = AnsiSequences.ESC + "M";
+
     [Fact]
     public void IDN_ReturnsIndexEscapeSequence()
     {
-        AnsiSequences.IDN().ShouldBe("\u001bD");
+        AnsiSequences.IDN().ShouldBe(IndexEscapeSequence);
     }
 
     [Fact]
     public void NEL_ReturnsNextLineEscapeSequence()
     {
-        AnsiSequences.NEL().ShouldBe("\u001bE");
+        AnsiSequences.NEL().ShouldBe(NextLineEscapeSequence);
     }
 
     [Fact]
     public void RI_ReturnsReverseIndexEscapeSequence()
     {
-        AnsiSequences.RI().ShouldBe("\u001bM");
+        AnsiSequences.RI().ShouldBe(ReverseIndexEscapeSequence);
     }
 }
